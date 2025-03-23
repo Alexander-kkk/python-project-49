af49f4d9e1b1e5e3a8f826c1fab9070a0a3c6621
Alexander <alexanderkuntsevo@gmail.com>
2025-03-23 13:18:05 +0300

    Add brain_games/brain_even_game_logic.py and brain_games/scripts/brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,10 @@
+import brain_games.brain_even_game_logic
+
+
+def main():
+    brain_games.brain_even_game_logic.welcome_user()
+    brain_games.brain_even_game_logic.display_questions_and_get_answers()
+
+
+if __name__ == "__main__":
+    main()
