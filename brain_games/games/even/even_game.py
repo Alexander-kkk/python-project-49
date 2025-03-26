dfdf7857b6b8b0d4e9832003d404d778b9587df3
Alexander <alexanderkuntsevo@gmail.com>
2025-03-27 00:48:08 +0300

    fix issues even_game.py

--- brain_games/games/even/even_game.py
+++ brain_games/games/even/even_game.py
@@ -20,11 +20,12 @@ def get_number_and_true_answer():
 def even_game():
     brain_games.games.engine.welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    count_of_correct_answers = 1
+    count_of_correct_answers = 0
     for i in range(brain_games.games.engine.count_of_rounds):
         number, true_answer = get_number_and_true_answer()
         brain_games.games.engine.print_question(number)
         answer = brain_games.games.engine.get_answer()
-        brain_games.games.engine.comparison(answer, true_answer, count_of_correct_answers)  # noqa: E501
+        brain_games.games.engine.comparison(answer, true_answer)
         count_of_correct_answers += 1
+        brain_games.games.engine.congratulation(count_of_correct_answers)
 
