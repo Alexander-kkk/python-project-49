e9d4d6ac5336c31b8930f3ba4b090cb9b6673008
Alexander <alexanderkuntsevo@gmail.com>
2025-03-26 18:47:23 +0300

    Add engine.py

---
+++ brain_games/games/engine.py
@@ -0,0 +1,32 @@
+import prompt
+
+count_of_rounds = 3
+
+
+def welcome_user():
+    print('Welcome to the Brain Games!')
+    global name
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+
+
+def print_question(number): 
+    print(f'Question: {number}')
+
+
+def get_answer():
+    answer = prompt.string('Your answer: ')
+    return answer
+
+
+def comparison(answer, true_answer, count_of_correct_answers):
+    if count_of_correct_answers == count_of_rounds:
+        print(f'Congratulations, {name}!')
+        exit()
+    if answer.lower() == true_answer:
+        print('Correct!')
+    else:
+        print(f"'{answer}' is wrong answer ;(.")
+        print(f"Correct answer was '{true_answer}'")
+        print(f"Let's try again, {name}")
+        exit()
