1a12b26c25c63e4743756db91f6d6125159f74d3
Alexander <alexanderkuntsevo@gmail.com>
2025-03-26 18:48:28 +0300

    Add even-game.py

---
+++ brain_games/games/even/even_game.py
@@ -0,0 +1,30 @@
+from random import randint
+
+import brain_games.games.engine
+
+
+def get_random_number():
+    return randint(1, 100)
+
+
+def is_even(number):
+    return number % 2 == 0
+
+
+def get_number_and_true_answer():
+    number = get_random_number()
+    true_answer = 'yes' if is_even(number) else 'no'
+    return number, true_answer
+
+
+def even_game():
+    brain_games.games.engine.welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    count_of_correct_answers = 1
+    for i in range(brain_games.games.engine.count_of_rounds):
+        number, true_answer = get_number_and_true_answer()
+        brain_games.games.engine.print_question(number)
+        answer = brain_games.games.engine.get_answer()
+        brain_games.games.engine.comparison(answer, true_answer, count_of_correct_answers)
+        count_of_correct_answers += 1
+
