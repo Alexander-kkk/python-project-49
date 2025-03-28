0ac84b5882362bab92f20ebe5991c429120663c5
Alexander <alexanderkuntsevo@gmail.com>
2025-03-28 14:01:44 +0300

    Add prime_game.py and brain_prime.py

---
+++ brain_games/games/prime/prime_game.py
@@ -0,0 +1,23 @@
+from random import randint
+
+description = '"yes" if given number is prime. Otherwise answer "no"'
+count_of_divisors_prime_number = 2
+
+
+def get_random_number():
+    return randint(1, 20)
+
+
+def is_prime(number):
+    count = 0
+    for i in range(1, number + 1):
+        if number % i == 0:
+            count += 1
+    if count == count_of_divisors_prime_number:
+        return True
+
+
+def generate_number_and_true_answer():
+    number = get_random_number()
+    true_answer = 'yes' if is_prime(number) else 'no'
+    return number, true_answer
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,10 @@
+from brain_games.games.engine import play_game
+from brain_games.games.prime import prime_game
+
+
+def main():
+    play_game(prime_game)
+    
+    
+if __name__ == "__main__":
+    main()
